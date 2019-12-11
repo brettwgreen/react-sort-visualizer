db55a955d1b9d5d7158ff5abb966efa89d2342f0
brettwgreen <bw_green@yahoo.com>
2019-12-11 15:11:13 -0500

    Updates
    gh pages

---
+++ precache-manifest.09b4044fa184091e8ef6a04a1509dafd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55f9780e143c3074ad2ed304516dcb3",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "1628616af8a437a24138",
+    "url": "/react-gh-pages/static/css/main.f1e51148.chunk.css"
+  },
+  {
+    "revision": "29564c49667ec60bc92c",
+    "url": "/react-gh-pages/static/js/2.d8004e9f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.d8004e9f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1628616af8a437a24138",
+    "url": "/react-gh-pages/static/js/main.c3fe9149.chunk.js"
+  },
+  {
+    "revision": "a1a1c33cbc66e72b88da",
+    "url": "/react-gh-pages/static/js/runtime-main.b3a8b867.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
